ad3bd37820cad3f24e8fa1f9db92fa5774e7a400
rigelrozanski <rigel.rozanski@gmail.com>
2016-12-14 13:44:39 -0500

    updating remainder of tests

--- tree_test.go
+++ tree_test.go
@@ -33,36 +33,71 @@ func TestAVLTREE(t *testing.T) {
 		}
 	}
 
-	printErr(tree.Add([]byte("keyOne"), []byte("valueOne")))
+	//some expected tree forms:
+	// a   a   b      b       b       d      d
+	//    /   / \    / \     / \     / \    / \
+	//   b   a   c  a   c   a   d   b   e  c   e
+	//                   \     / \   \
+	//                    d   c   e   c
+
+	printErr(tree.Add([]byte("a"), []byte("vA")))
 	heightTest(0)
-	printErr(tree.Add([]byte("keyTwo"), []byte("valueTwo")))
+	printErr(tree.Add([]byte("b"), []byte("vB")))
 	heightTest(1)
-	printErr(tree.Add([]byte("keyThree"), []byte("valueThree")))
+	printErr(tree.Add([]byte("c"), []byte("vC")))
 	heightTest(1)
-	printErr(tree.Add([]byte("keyFour"), []byte("valueFour")))
+	printErr(tree.Add([]byte("d"), []byte("vD")))
+	heightTest(2)
+	printErr(tree.Add([]byte("e"), []byte("vE")))
 	heightTest(2)
 
 	//Test retrieving saved values
-	retrieveTest := func(key, expectedVal string) {
+	retrieveTest := func(key, expectedVal string, expectedExists bool) {
 		recievedVal, err := tree.Get([]byte(key))
-		printErr(err)
-		if bytes.Compare(recievedVal, []byte(expectedVal)) != 0 {
-			t.Errorf("bad expected %v recieved %v ", expectedVal, string(recievedVal[:]))
+		if expectedExists {
+			printErr(err)
+			if bytes.Compare(recievedVal, []byte(expectedVal)) != 0 {
+				t.Errorf("bad expected %v recieved %v ", expectedVal, string(recievedVal[:]))
+			}
+		} else {
+			if err == nil {
+				t.Errorf("expected to receive an error when attempting to retrieve non-existent value for key ", key)
+			}
 		}
 	}
 
-	retrieveTest("keyOne", "valueOne")
-	retrieveTest("keyTwo", "valueTwo")
-	retrieveTest("keyThree", "valueThree")
-	retrieveTest("keyFour", "valueFour")
+	retrieveTest("a", "vA", true)
+	retrieveTest("b", "vB", true)
+	retrieveTest("c", "vC", true)
+	retrieveTest("d", "vD", true)
+	retrieveTest("e", "vE", true)
 
 	//Test adding a duplicate value
+	expErr := tree.Add([]byte("a"), []byte("vA"))
+	if expErr == nil {
+		t.Errorf("expected to receive an error when attempting to add duplicate values")
+	}
 
 	//Test updating an existing value
+	printErr(tree.Update([]byte("a"), []byte("vAA")))
+	retrieveTest("a", "vAA", true)
 
 	//Test updating a non existent value
+	expErr = tree.Update([]byte("z"), []byte("vZ"))
+	if expErr == nil {
+		t.Errorf("expected to receive an error when attempting to update non-existent value")
+	}
 
 	//Test removing saved values from the tree
+	printErr(tree.Remove([]byte("a")))
+	heightTest(2)
+	printErr(tree.Remove([]byte("b")))
+	heightTest(1)
 
-	//Test retrieval of saved values
+	//Test bad retrieval of old saved value
+	retrieveTest("a", "", false)
+	retrieveTest("b", "", false)
+	retrieveTest("c", "vC", true)
+	retrieveTest("d", "vD", true)
+	retrieveTest("e", "vE", true)
 }
